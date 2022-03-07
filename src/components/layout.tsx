294c28e3a71840306bcd535254ef9ab3f27f4fcf
Kevin Woodfield <kcwoodfield@gmail.com>
2022-03-07 15:02:34 -0600

    committing

---
+++ src/components/layout.tsx
@@ -0,0 +1,11 @@
+// import { FunctionComponent, ReactNode } from "react";
+// import Link from "next/link";
+// import { useAuth } from "src/auth/useAuth";
+
+interface IProps {
+    main: ReactNode;
+}
+
+const Layout: FunctionComponent<IProps> = ({ main }) => {
+    return <div>Layout</div>;
+}
